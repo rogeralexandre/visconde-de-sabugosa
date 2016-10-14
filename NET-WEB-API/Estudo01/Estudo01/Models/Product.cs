e0b152d9ca4b16298e03ea0bfd7cdb4e57937fce
Roger Alexandre Pinto - Capgemini <BRX0000097@latam2.prosegur.local>
2016-10-14 12:24:11 -0300

    exemplo tirado do site da microsoft - está funcionando

---
+++ NET-WEB-API/Estudo01/Estudo01/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Estudo01.Models
+{
+    public class Product
+    {
+        // Entidade de produtos.
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
