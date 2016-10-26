9004a62488dca9a1c0c049242bcf39ee1eafa486
Roger Alexandre Pinto <rogeralexandre@gmail.com>
2016-10-26 13:54:48 -0200

    exemplo de CONTRACT FIRST

---
+++ contract-first-sample/MyServiceMock/MyServiceMock/IVIGIA-COMERCIAL-INPUT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyServiceMock
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IVIGIA_COMERCIAL_INPUT" in both code and config file together.
+    [ServiceContract]
+    public interface IVIGIA_COMERCIAL_INPUT
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
---
+++ contract-first-sample/MyServiceMock/MyServiceMock/VIGIA-COMERCIAL-INPUT.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyServiceMock
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "VIGIA_COMERCIAL_INPUT" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select VIGIA-COMERCIAL-INPUT.svc or VIGIA-COMERCIAL-INPUT.svc.cs at the Solution Explorer and start debugging.
+    //public interface WSVIGIACOMERCIALInputportType
+    // IVIGIA_COMERCIAL_INPUT
+    public class VIGIA_COMERCIAL_INPUT : WSVIGIACOMERCIALInputportTypeClient
+    {
+        new public VigiaReply1 MaestroSubescalas(MaestroSubescalasRequest1 request)
+        {
+            return base.Channel.MaestroSubescalas(request);
+        }
+
+    }
+}
