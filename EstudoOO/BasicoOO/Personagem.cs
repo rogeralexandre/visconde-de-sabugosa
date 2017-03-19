cbf7ad462951f3db58becdc30a78eaea3d923db9
rogeralexandre@gmail.com <rogeralexandre@gmail.com>
2017-03-19 12:45:19 -0300

    estudo básico de classe e uso de equals

---
+++ EstudoOO/BasicoOO/Personagem.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicoOO
+{
+    public class Personagem
+    {
+        //atributos
+        string nome;
+        string cor;
+        int quantidadeDeCogumelos;
+        float altura;
+        string tipoFisico;
+        bool possuiBigode;
+
+        // GETs e SETs
+        // estilo puxado do livro de OO da casa do código.
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value;  }
+        }
+
+        public string Cor
+        {
+            get { return cor; }
+            set { cor = value; }
+        }
+
+        // gerado ao fazer o build quando o atributo não tinha get,set cliquei com o direito e gerou;
+        public int QuantidadeDeCogumelos { get => quantidadeDeCogumelos; set => quantidadeDeCogumelos = value; }
+        public float Altura { get => altura; set => altura = value; }
+        public string TipoFisico { get => tipoFisico; set => tipoFisico = value; }
+        public bool PossuiBigode { get => possuiBigode; set => possuiBigode = value; }
+
+        // método construtor
+        public Personagem()
+        {
+
+        }
+
+        // métodos
+        void Pular()
+        {
+
+        }
+
+        void PegarCogumelo(Cogumelo cogumelo)
+        {
+
+        }
+
+        Bolafogo AtirarFogo()
+        {
+            return new Bolafogo();
+        }
+
+        // método destrutor
+        ~Personagem()
+        {
+
+        }
+
+        //Equals
+        public override bool Equals(object obj)
+        {
+            if (obj is Personagem)
+            {
+                Personagem p = obj as Personagem;
+                return this.nome.Equals(p.nome);
+            }
+            return false;
+        }
+
+    }
+
+    class Cogumelo
+    {
+
+    }
+
+    class Bolafogo
+    {
+
+    }
+}
+    
---
+++ EstudoOO/BasicoOO/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicoOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem personagem = new Personagem();
+
+            personagem.PossuiBigode = true;
+            personagem.Altura = 1.5F;
+            personagem.QuantidadeDeCogumelos = 0;
+            personagem.TipoFisico = "parece um encanador italiano";
+            personagem.Nome = "Mario";
+            personagem.Cor = "Vermelho";
+
+            Personagem personagem2 = new Personagem();
+
+            if (personagem.Equals(personagem2))
+            {
+                Console.WriteLine("Os dois personagens são iguais.");
+            }
+            else
+            {
+                Console.WriteLine("Os personagens são diferentes.");
+            }
+
+            personagem2.Nome = personagem.Nome;
+            if (personagem.Equals(personagem2))
+            {
+                Console.WriteLine("Agora os dois personagens são iguais.");
+            }
+            else
+            {
+                Console.WriteLine("Os personagens ainda são diferentes.");
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para continuar.");
+            Console.ReadKey();
+        }
+    }
+}
